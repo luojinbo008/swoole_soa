29792f1b08afe7f26bdcc3dc8ee08d7d05a48b94
luojinbo <luojinbo>
2018-10-31 10:46:01 +0800

    调整

--- lib/factory/cache.php
+++
@@ -1,6 +0,0 @@
-<?php
-$configs = \Swoole\Swoole::$php->config['cache'];
-if (empty($configs[\Swoole\Swoole::$php->factory_key])) {
-    throw new Swoole\Exception\Factory("cache->".\Swoole\Swoole::$php->factory_key." is not found.");
-}
-return Swoole\Factory::getCache(\Swoole\Swoole::$php->factory_key);
